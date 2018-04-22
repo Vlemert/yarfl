45462e48663e4f906090f2b938874a63fa1768e0
Niels Vleeming <niels.vleeming@greenhousegroup.com>
2018-04-22 16:33:25 +0200

    Add onBlur tests

--- src/__tests__/test.jsx
+++ src/__tests__/test.jsx
@@ -160,5 +160,67 @@ describe('Yarfl', () => {
         expect(updatedEmailArgs.invalid).toBe(true);
       });
     });
+
+    describe('onBlur', () => {
+      test('handled and causes rerender', () => {
+        const renderEmail = jest.fn(() => null);
+
+        TestRenderer.create(
+          <MyForm>
+            {({}) => (
+              <React.Fragment>
+                <MyForm.Field name="email">{renderEmail}</MyForm.Field>
+              </React.Fragment>
+            )}
+          </MyForm>
+        );
+
+        const emailArgs = renderEmail.mock.calls[0][0];
+        const emailOnBlur = emailArgs.input.onBlur;
+
+        emailOnBlur({
+          target: {
+            value: 'changed text'
+          }
+        });
+
+        expect(renderEmail.mock.calls.length).toBe(2);
+        expect(renderEmail.mock.calls[1][0].input.value).toBe('changed text');
+      });
+
+      test('causes validate to be called', () => {
+        const renderEmail = jest.fn(() => null);
+        const validateEmail = jest.fn();
+
+        TestRenderer.create(
+          <MyForm>
+            {({}) => (
+              <React.Fragment>
+                <MyForm.Field name="email" validate={validateEmail}>
+                  {renderEmail}
+                </MyForm.Field>
+              </React.Fragment>
+            )}
+          </MyForm>
+        );
+
+        const emailArgs = renderEmail.mock.calls[0][0];
+        const emailOnBlur = emailArgs.input.onBlur;
+
+        validateEmail.mockImplementationOnce(() => 'test error');
+        emailOnBlur({
+          target: {
+            value: 'changed text'
+          }
+        });
+
+        expect(validateEmail.mock.calls.length).toBe(2);
+        expect(validateEmail).toBeCalledWith('changed text');
+        const updatedEmailArgs = renderEmail.mock.calls[1][0];
+        expect(updatedEmailArgs.error).toBe('test error');
+        expect(updatedEmailArgs.valid).toBe(false);
+        expect(updatedEmailArgs.invalid).toBe(true);
+      });
+    });
   });
 });
