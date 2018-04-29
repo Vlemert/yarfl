eb6bf64fcf223b635ccf18d8e7f27b20781d4672
Niels Vleeming <niels.vleeming@greenhousegroup.com>
2018-04-29 21:55:51 +0200

    Remove outdated todo list

--- src/index.js
+++ src/index.js
@@ -5,17 +5,6 @@ import State from './components/state';
 import Field from './components/field';
 import SubField from './components/sub-field';
 
-/**
- * GENERIC TODO LIST:
- *
- * - finish submit (submitting state, error handling, etc)
- * - errors on form level and a component to retrieve them
- * - component that retrieves all form values
- * - readonly field component?
- * - form sections (nested context providers I guess?)
- *
- */
-
 export default {
   Form,
   Field,
